f5e68103071e307b2b76c04cbc32bbd6dc2477ee
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-06-26 16:11:51 +0530

    implemented gsap hooks

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+  useGSAP(() =>{
+    gsap.from('#green-box', {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+      ease: 'powerOne.inOut'
+    })
+  }, [])
 
   return (
     <main>
--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
+  useGSAP(() =>{
+    gsap.to('#blue-box', {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+      ease: 'bounce.in'
+    })
+  }, [])
 
   return (
     <main>
